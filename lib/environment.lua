241a93ceabb502457fc3be0ef049618a19771e81
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-07-27 22:00:27 +0900

    store coordinate history and check novelty

--- lib/environment.lua
+++ lib/environment.lua
@@ -17,6 +17,8 @@ function Environment.new()
   self.maze = Maze(self.size)
   self.current_coordinate = { 0, 0 }
   self.move_count = 0
+  self.history = { { 0, 0 } }
+  self.novelty = 0
   return self
 end
 
@@ -47,4 +49,22 @@ function Environment:move(direction)
     self.current_coordinate = neighbor[direction]
   end
   self.move_count = self.move_count + 1
+  self:check_novelty()
+end
+
+function Environment:check_novelty()
+  local flag = false
+  for _, v in ipairs(self.history) do
+    -- Lua does not have an operator to compare table values
+    if v[1] == self.current_coordinate[1] and v[2] == self.current_coordinate[2] then
+      flag = true
+      break
+    end
+  end
+  if flag then
+    self.novelty = 0
+  else
+    self.novelty = 10
+    self.history[#self.history + 1] = self.current_coordinate
+  end
 end
